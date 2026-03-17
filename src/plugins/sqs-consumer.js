b8edb53309bf722dcf47f95ba6265df3f87e409d
SealDebolinaCog <debolina.seal@cognizant.com>
2026-03-17 13:53:09 +0000

    lint fix

--- src/plugins/sqs-consumer.js
+++ src/plugins/sqs-consumer.js
@@ -4,16 +4,15 @@ export default {
   name: 'sqsConsumer',
   register: async function (server) {
     const abortController = new AbortController()
-    let keepRunning = true
 
     // Expose controller so other parts of system can trigger stop if needed
     server.app.sqsAbortController = abortController
 
     // Background loop
     const poll = async () => {
-      while (keepRunning) {
+      while (!abortController.signal.aborted) {
         await main(server, undefined, abortController.signal)
-        await new Promise((res) => setTimeout(res, 500)) // small sleep
+        await new Promise((resolve) => setTimeout(resolve, 500)) // small sleep
       }
     }
 
@@ -22,7 +21,6 @@ export default {
     // Graceful shutdown
     server.events.on('stop', () => {
       console.log('Hapi server stopping → halting SQS consumer...')
-      keepRunning = false
       abortController.abort() //Stop long polling immediately and Stop background loop
       sqsClient.destroy() //Close open HTTP sockets,Force Node to exit cleanly and Prevent memory leaks over time
     })
