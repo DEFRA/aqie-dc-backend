31d11331761ad9715c8fa7e188a3ae5fac245b62
Cora-Jane <cora-jane.meade@cognizant.com>
2026-08-13 14:47:37 +0100

    Remove duplicate call and comment

--- src/sqs/client.js
+++ src/sqs/client.js
@@ -77,7 +77,6 @@ export const main = async (server, queueUrl, abortSignal) => {
     // -------------------------------
     for (const message of Messages) {
       try {
-        await ingestSqsMessageViaRoute(server, message)
         await createNewRecord(message, server)
       } catch (err) {
         logger.error('API call failed. MessageId:', message.MessageId)
