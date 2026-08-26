070f78ceae357f0a8f3ace60bf330dd35b840776
Cora-Jane <cora-jane.meade@cognizant.com>
2026-08-26 16:28:32 +0100

    Add missing field for endpoint

--- src/controllers/applications-controller.js
+++ src/controllers/applications-controller.js
@@ -450,7 +450,8 @@ async function getApplicationsWithSummary(
           .map((appliance) => ({
             id: appliance._id,
             modelName: appliance.modelName
-          }))
+          })),
+        reviewedBy: app.reviewedBy
       }
 
       if (app.status === 'new') {
