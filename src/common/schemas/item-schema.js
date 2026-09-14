19adf29e34b3f9a3ded7b02efc9dd2de826cab48
Cora-Jane <cora-jane.meade@cognizant.com>
2026-09-14 12:22:51 +0100

    Corrected miskate additional Commnets -> conditions

--- src/common/schemas/item-schema.js
+++ src/common/schemas/item-schema.js
@@ -113,10 +113,10 @@ export const applianceSchema = Joi.object({
     rated: Joi.number().optional().description('Smoke emission output - rated'),
     low: Joi.number().optional().description('Smoke emission output - low')
   }).optional(),
-  additionalComments: Joi.string()
+  additionalConditions: Joi.string()
     .optional()
     .description(
-      'Additional comments previously referred to as air control modifications'
+      'Additional conditions previously referred to as air control modifications'
     ),
   instructionManual: Joi.object({
     title: Joi.string().optional().description('Instruction manual title'),
--- src/sample-data/appliance-example.js
+++ src/sample-data/appliance-example.js
@@ -41,7 +41,7 @@
     rated: 2.3,
     low: 1.1
   },
-  additionalComments: 'Modified secondary air controls',
+  additionalConditions: 'Modified secondary air controls',
   instructionManual: {
     title: 'Manual X',
     date: '2026-02-03',
