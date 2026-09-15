ec88e763fa12e2558b92d0cb259f756e47b75d7d
rajeshnadella <rajesh.n4@cognizant.com>
2026-09-15 20:34:50 +0100

    changes done for additional conditions

--- src/controllers/appliance-review-controller.js
+++ src/controllers/appliance-review-controller.js
@@ -31,6 +31,7 @@ async function getApplianceReview(db, id, logger) {
           applicationId: 1,
           permittedFuels: 1,
           isPermittedToBurnWood: 1,
+          additionalConditions: 1,
           technicalReview: 1,
           _id: 0
         }
@@ -88,9 +89,9 @@ async function recordApplianceCheck(db, id, check, result, logger, data) {
 
     const updates = data
       ? {
-          ...data,
-          technicalReview: { [group]: { [check]: result } }
-        }
+        ...data,
+        technicalReview: { [group]: { [check]: result } }
+      }
       : { technicalReview: { [group]: { [check]: result } } }
 
     if (item.technicalReview?.status === 'new') {
